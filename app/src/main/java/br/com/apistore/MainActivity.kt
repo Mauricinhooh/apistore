e27a69c849cd0f8b5fe7533b49d3109c7ce001c6
Mauricinhooh <mauricio.contato2015@gmail.com>
2019-10-19 00:41:55 -0300

    Layout do item do carrosel

--- app/src/main/java/br/com/apistore/MainActivity.kt
+++ app/src/main/java/br/com/apistore/MainActivity.kt
@@ -2,11 +2,40 @@ package br.com.apistore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.ActionBar
+import androidx.viewpager.widget.PagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupCarousel()
+        supportActionBar?.elevation = 0f
+        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
     }
+
+    private fun setupCarousel() {
+        icvStore.adapter = object : PagerAdapter() {
+            override fun isViewFromObject(view: View, `object`: Any): Boolean = view == `object`
+            override fun getCount(): Int = 6
+            override fun instantiateItem(container: ViewGroup, position: Int): Any {
+                val view = LayoutInflater.from(this@MainActivity)
+                    .inflate(R.layout.carousel_item, container, false)
+                view.findViewById<Button>(R.id.itemButton).text = position.toString()
+                container.addView(view)
+                return view
+            }
+
+            override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+
+            }
+        }
+    }
+
 }
